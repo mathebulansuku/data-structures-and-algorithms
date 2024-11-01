531f2c771d3cd2815656c71f82fd58dcf644861f
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-11-01 14:22:10 +0200

    created a list to test the function

--- linear_search.py
+++ linear_search.py
@@ -9,3 +9,8 @@ def verify(index):
     print("Target found at index: ", index)
   else:
     print("Target not found in list")
+
+numbers = [1,2,3,4,5,6,7,8,9,10] 
+
+result = linear_search(numbers, 12)
+verify(result)
