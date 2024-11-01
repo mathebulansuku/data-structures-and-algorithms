ff454c36b3994eb24089046c7a2b75b36af0d9b1
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-11-01 15:04:24 +0200

    verify the binary search

--- recursive_binary_search.py
+++ recursive_binary_search.py
@@ -12,3 +12,15 @@ def recursive_binary_search(list, target):
       else:
         return recursive_binary_search(list[:midpoint], target)
     
+
+
+def verify(result):
+  print("Target found: ", result)
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+result = recursive_binary_search(numbers, 12)
+verify(result)
+
+result = recursive_binary_search(numbers, 6)
+verify(result)
