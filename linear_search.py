96816f892a06de2f79911be371dbb11547d9d82a
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-11-01 14:18:39 +0200

    verify if index in list is found

--- linear_search.py
+++ linear_search.py
@@ -3,3 +3,9 @@ def linear_search(list, target):
     if list[i] == target:
       return i
   return None
+
+def verify(index):
+  if index is not None:
+    print("Target found at index: ", index)
+  else:
+    print("Target not found in list")
