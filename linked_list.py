a2835c6cc3ba2a2fe6541d47f5bb78cb33bae32c
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-11-09 23:16:25 +0200

    Created an insert method that allows you to insert a node at any index/position of a list

--- linked_list.py
+++ linked_list.py
@@ -50,8 +50,30 @@ class LinkedList():
     return None
   
   def insert(self, data,index):
-    pass
+    """
+    Inserts a new Node containing data at index position
+    Insertion takes O(1) time but finding the node at the insertion point 
+    takes O(n) time.
+    """
 
+    if index == 0:
+      self.add(data) # class the add method to insert node at the head of the list
+
+    if index > 0:
+      new = Node(data)
+
+      position = index
+      current = self.head
+
+    while position > 1:
+      current = current.next_node
+      position -= 1
+
+    prev_node = current 
+    next_node = current.next_node
+
+    prev_node.next_node = new
+    new.next_node = next_node
 
   def __repr__(self):
     node = []
