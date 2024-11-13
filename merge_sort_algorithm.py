1e39b5579be7fe764477684a22d19e7a5ee1dfdd
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-11-13 07:04:58 +0200

    Create a merge function, that merges the 2 sublists

---
+++ merge_sort_algorithm.py
@@ -0,0 +1,31 @@
+# Merge sort is a sorting algorithm that splits the list into smalleer sublists, sort them, and then merges them back togetheer to form the sorted list. It has a time complexity of O(nlog n).
+
+def merge_sort(list):
+  
+  """
+  STEPS:
+
+  1. Divide: Find the midpoint of the list and divide into sublists
+  2. Conquer: Recursively sort the sublists created in previous
+  3. Combine: Merge the sorted sublists created in previous step
+  """
+
+  if len(list) <= 1:
+    return list
+  
+  left_half, right_half = split(list) #Divide
+  left  = merge_sort(left_half) #conquer
+  right = merge_sort(right_half)#conquer
+
+  return merge(left,right)
+
+def split(list): #This function divides the unsorted list at midpoint into sublists
+
+  midpoint = len(list)//2
+  left = list[:midpoint]
+  right = list[midpoint:]
+
+  return left, right 
+
+  def merge(left, right): #Function merges 2 lists, sorting them in the process, and returns a new merged list
+    pass
