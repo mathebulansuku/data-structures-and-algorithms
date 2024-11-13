38abd6d574fead2f3e982aa9d43888f7070d6b3b
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-11-13 09:45:26 +0200

    Created a merge_sort function that splits, sorts and merges the linked_list

--- sort_linked_list.py
+++ sort_linked_list.py
@@ -1,2 +1,28 @@
 from linked_list_ds import LinkedList 
 
+def merge_sort(linked_list):
+  """
+  Sorts a linked list in ascending order
+  -Recursively divide the linked list into sublists containing a single node
+  -Repeatedly merge the sublists to produce sorted sublists until one remains
+
+  Returns the sorted linked list
+  """
+  if linked_list.size() == 0 or linked_list.size() == 1:
+    return linked_list
+  
+  left_half,right_half = split(linked_list)
+
+  left = merge_sort(left_half)
+  right = merge_sort(right_half)
+
+  return merge(left,right)
+
+
+
+
+def split():
+  pass
+
+def merge():
+  pass
