34403189a074fb9974d4a59c2b3ac5be42978bb4
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-11-13 10:29:53 +0200

    node_at_index function returns a node a given index

--- linked_list_ds.py
+++ linked_list_ds.py
@@ -98,6 +98,19 @@ class LinkedList():
         current = current.next_node
 
     return current
+  
+  def node_at_index(self,index): #Method return node at a given index
+    if index == 0:
+      return self.head
+    else:
+      current = self.head
+      position = 0
+
+      while position < index:
+        current = current.next_node
+        position += 1
+
+      return current
 
 
         
