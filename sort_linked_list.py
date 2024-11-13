601caff590c1e183ac34b17b9ea64d22385ef061
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-11-13 09:58:49 +0200

    created the split and merge function

--- sort_linked_list.py
+++ sort_linked_list.py
@@ -21,8 +21,38 @@ def merge_sort(linked_list):
 
 
 
-def split():
-  pass
+def split(linked_list):
+  
+  midpoint = len(linked_list())//2
 
-def merge():
-  pass
+  left = linked_list[midpoint:]
+  right = linked_list[:midpoint]
+
+  return left,right
+
+
+def merge(left, right):
+  
+  merged_list = []
+  i = 0
+  j = 0
+
+  while i < len(left) and j < len(right):
+    if left[i] < right[j]:
+      merged_list.append(left[i])
+      i += 1
+    else:
+      merged_list.append(right[j])
+      j+= 1
+
+  while i < len(left):
+    merged_list.append(left[i])
+    i +=1
+
+  while j < len(right):
+    merged_list.append(right[j])
+    j +=1
+
+
+  return merged_list
+
