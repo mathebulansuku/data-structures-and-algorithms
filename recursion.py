c7f5facb7222080919b25b45f6e4eab3d8599793
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-11-16 22:47:59 +0200

    Recursion example

--- recursion.py
+++ recursion.py
@@ -1,11 +1,9 @@
 # Recursion is the ability of a function to call itself
 
 def sum(numbers):
-  total = 0
-
-  for number in numbers:
-    total += number # total = total + number
-  return total
-
+  if not numbers:
+    return 0
+  remaining_sum = sum(numbers[1:])
+  return numbers[0] + remaining_sum
 
 print(sum([1,2,7,9]))
