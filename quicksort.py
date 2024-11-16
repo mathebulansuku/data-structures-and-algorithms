6b1a3584812045805921896cc3e3a098e1a7eb96
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-11-17 00:54:28 +0200

    Loop through each item, and add item to the correct list

--- quicksort.py
+++ quicksort.py
@@ -7,6 +7,13 @@ def quick_sort(list):
   pivot = list[0]
   pivot_less = []
   pivot_plus = []
+
+  for item in list[1:]: #We loop through every item in list excluding the pivot value list[0]
+    if item <= pivot: #This adds items to the specific lists(pivot_less and pivot_plus)
+      pivot_less.append(item) 
+    else:
+      pivot_plus.append(item)
+
   
 sorted_list = quick_sort(list)
 print(sorted_list)
