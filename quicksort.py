608ada54ca0f26ad03c3756fb99bae12e7770f8b
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-11-17 00:48:22 +0200

    breakdown list into pivot, pivot+, pivot-

--- quicksort.py
+++ quicksort.py
@@ -4,6 +4,9 @@
 def quick_sort(list):
   if len(list) <=1: #base case 
     return list
+  pivot = list[0]
+  pivot_less = []
+  pivot_plus = []
   
 sorted_list = quick_sort(list)
 print(sorted_list)
