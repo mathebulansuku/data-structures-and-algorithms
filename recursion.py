bff51a3a938fa316daf22492f145675e5eafd3f6
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-11-17 00:34:17 +0200

    Save recursion.py file

--- recursion.py
+++ recursion.py
@@ -1,7 +1,7 @@
 # Recursion is the ability of a function to call itself
 
 def sum(numbers):
-  if not numbers:
+  if not numbers: #if list is empty return 0
     return 0
   remaining_sum = sum(numbers[1:])
   return numbers[0] + remaining_sum
